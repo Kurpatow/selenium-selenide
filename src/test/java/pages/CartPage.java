378dd6a78a1a84bcea0456662ba2eb4043f4f6ce
y0k0s <kurpatowph@yandex.ru>
2026-06-17 22:15:13 +0300

    .

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -1,4 +1,13 @@
 package pages;
 
-public class CartPage {
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class CartPage extends  BasePage {
+
+    public void shouldContainProduct(String productName) {
+
+        $x("//div[text()='" + productName + "']")
+                .shouldBe(visible);
+    }
 }
--- src/test/java/tests/AddToCartTest.java
+++ src/test/java/tests/AddToCartTest.java
@@ -1,4 +1,32 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+import pages.CartPage;
+import pages.LoginPage;
+import pages.MainPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class AddToCartTest {
+
+    @Test
+    void  addProductToCartTest() {
+
+        open("https://www.saucedemo.com/");
+
+        String productName = "Sauce Labs Backpack";
+
+        LoginPage loginPage = new LoginPage();
+
+        MainPage mainPage =
+                loginPage.login(
+                        "standard_user",
+                        "secret_sauce");
+
+        mainPage.getProductByTitle(productName).addToCart();
+
+        CartPage cartPage = mainPage.openCart();
+
+        cartPage.shouldContainProduct(productName);
+    }
 }
